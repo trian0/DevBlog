ffce42725947b6f40e868bc6796e1ef6a84f300d
Thales Rian <thalessantos.hrzon@gmail.com>
2022-06-02 03:56:07 -0300

    Finaliza home e cria modal e tela de detalhes

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,18 +1,156 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState, useLayoutEffect} from 'react';
+import {View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity, ScrollView, Share, Modal} from 'react-native';
+
+import { useNavigation, useRoute } from '@react-navigation/native';
+import api from '../../services/api';
+import {Feather, Entypo} from '@expo/vector-icons';
+import LinkWeb from '../../components/LinkWeb';
 
 export default function Detail(){
+
+    const route = useRoute();
+    const navigation = useNavigation();
+
+    const [post, setPost] = useState({})
+    const [links, setLinks] = useState([])
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [openLink, setOpenLink] = useState({});
+
+    useEffect( () => {
+        
+        async function getPosts(){
+            const response = await api.get( `api/posts/${route.params?.id}?populate=cover,category,opcoes` )
+            setPost(response.data.data)
+            setLinks(response.data?.data?.attributes?.opcoes)
+        }
+
+        getPosts();
+    }, [])
+
+    useLayoutEffect( () => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity onPress={handleShare}>
+                    <Entypo name='share' size={25} color='#FFF' />
+                </TouchableOpacity>
+            )
+        })
+    }, [navigation, post])
+
+    async function handleShare(){
+        try {
+            const result = await Share.share({
+                message: `
+                    Confere esse post: ${post?.attributes?.title}
+
+                    ${post?.attributes?.description}
+
+                    Vi lá no app DevBlog!
+                `
+            })
+
+            if(result.action === Share.sharedAction){
+                if(result.activityType){
+                    console.log("ACITIVITY TYPE")
+                } else {
+                    console.log("COMPARTILHADO COM SUCESSO")
+                }
+            } else if(result.action === Share.dismissedAction){
+                console.log("MODAL FECHADO")
+            }
+        } catch(error){
+            console.log("error")
+        }
+    }
+
+    function handleOpenLink(Link){
+        setModalVisible(true);
+        setOpenLink(Link)
+    }
+
     return(
-        <View style = {styles.container}>
-            <Text>Detail Page</Text>
-        </View>
+        <SafeAreaView style = {styles.container}>
+            <Image 
+                resizeMode='cover'
+                style = {styles.cover} 
+                source = {{ uri: `http://192.168.15.7:1337${post.attributes?.cover?.data?.attributes?.url}` }}
+            />
+
+            <Text style={styles.title} >
+                {post?.attributes?.title}
+            </Text>
+
+            <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
+                
+                <Text style={styles.description}>
+                    {post?.attributes?.description}
+                </Text>
+
+                {links.length > 0 && (
+                    <Text style={styles.subTitle}>Links</Text>
+                )}
+
+                {links.map( Link => (
+                    <TouchableOpacity 
+                        key={Link.id}
+                        style={styles.linkButton}
+                        onPress={() => handleOpenLink(Link)}
+                    >
+                        <Feather name='link' color={'#1e4687'} size={14}/>
+                        <Text style={styles.linkText}>{Link.name}</Text>
+                    </TouchableOpacity>
+                ))}
+                
+            </ScrollView>
+
+            <Modal animationType='slide' visible={modalVisible} transparent={true}>
+                <LinkWeb 
+                    link={openLink?.url}
+                    title={openLink?.name}
+                    closeModal={() => setModalVisible(false)}
+                />
+            </Modal>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#FFF'
+    },
+    cover:{
+        width: '100%',
+        height: 230,
+    },
+    title:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 14,
+        marginTop: 18,
+        paddingHorizontal: 12,
+    },
+    content:{
+        paddingHorizontal: 12,
+    },
+    description:{
+        lineHeight: 20,
+    },
+    subTitle:{
+        fontWeight: 'bold',
+        marginTop: 14,
+        fontSize: 18,
+        marginBottom: 6,
+    },
+    linkButton:{
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        marginBottom: 8,
+    },
+    linkText:{
+        color: '#1e4687',
+        fontSize: 16,
+        marginLeft: 6,
     }
 })
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,116 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, SafeAreaView, FlatList} from 'react-native';
 
 import {useNavigation} from '@react-navigation/native';
 import {Feather} from '@expo/vector-icons';
 
+import api from '../../services/api';
+import CategoryItem from '../../components/CategoryItem';
+import {getFavorite, setFavorite} from '../../services/favorite';
+import FavoritePost from '../../components/FavoritePost';
+import PostItem from '../../components/PostItem';
+
 export default function Home(){
 
     const navigation = useNavigation();
+    const [categories, setCategories] = useState ([])
+    const [favCategory, setFavCategory] = useState ([])
+    const [posts, setPosts] = useState ([])
+
+    useEffect( () => {
+
+        async function loadData(){
+            await getListPosts();
+
+            const category = await api.get("api/categories?populate=icon")
+            setCategories(category.data.data)
+        }
+
+        loadData();
+
+    }, [])
+
+    useEffect( () => {
+
+        async function favorite(){
+            const response = await getFavorite()
+            setFavCategory(response);
+        }
+
+        favorite();
+
+    }, [])
+
+    async function getListPosts(){
+        const response = await api.get("api/posts?populate=cover&sort=createdAt:desc")
+
+        setPosts(response.data.data);
+    }
+
+    //Favoritando uma categoria
+    async function handleFavorite(id){
+        const response = await setFavorite(id)
+
+        setFavCategory(response);
+    }
 
     return(
         <SafeAreaView style = {styles.container}>
+
             <View style = {styles.header} >
                 <Text style = {styles.name} >DevBlog</Text>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={ () => navigation.navigate("Search")}>
                     <Feather name = "search" size = {24} color = '#FFF' />
                 </TouchableOpacity>
             </View>
+
+            <FlatList 
+                    showsHorizontalScrollIndicator={false}
+                    horizontal={true}
+                    contentContainerStyle={{ paddingRight: 12}}
+                    style={styles.categories}
+                    data={categories}
+                    keyExtractor={ (item) => String(item.id)}
+                    renderItem={ ({item}) => (
+                        <CategoryItem 
+                            data={item}
+                            favorite={ () => handleFavorite(item.id)}
+                        />
+                    )}
+            />
+
+            <View style={styles.main}>
+                
+                    {favCategory.length != 0 && (
+                        <FlatList 
+                            style={{ marginTop: 50, maxHeight: 100, paddingStart: 18 }}
+                            contentContainerStyle = {{ paddingEnd: 18}}
+                            data={favCategory}
+                            horizontal={true}
+                            showsHorizontalScrollIndicator={false}
+                            keyExtractor={ (item) => String(item.id) }
+                            renderItem={ ({item}) => <FavoritePost data={item}/>}
+                        />
+                    )}
+
+                    <Text 
+                    style={[
+                        styles.title,
+                        { marginTop: favCategory.length > 0 ? 14 : 46 }
+                    ]}>
+                        Conteúdos em alta
+                    </Text>
+
+                    <FlatList 
+                        style={{ flex: 1, paddingHorizontal: 18 }}
+                        showsVerticalScrollIndicator={false}
+                        data={posts}
+                        keyExtractor={(item) => String(item.id) }
+                        renderItem={ ({item}) => <PostItem data={item}/>}
+                    />
+            </View>
+
         </SafeAreaView>
     )
 }
@@ -38,5 +132,24 @@ const styles = StyleSheet.create({
         fontSize: 28,
         color: '#FFF',
         fontWeight: 'bold'
+    },
+    categories: {
+        maxHeight: 115,
+        backgroundColor: '#EFEFEF',
+        marginHorizontal: 18,
+        borderRadius: 8,
+        zIndex: 9
+    },
+    main:{
+        backgroundColor: '#FFF',
+        flex: 1,
+        marginTop: -30,
+    },
+    title:{
+        fontSize: 21,
+        paddingHorizontal: 18,
+        marginBottom: 14,
+        fontWeight: 'bold',
+        color: '#162133'
     }
 })
